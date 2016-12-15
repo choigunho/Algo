6ab852e1ac6ce40a4c39162e27b46c741c4550a3
gunho14.choi <gunho14.choi@samsung.com>
2016-12-15 19:57:14 +0900

    .

--- Practice/src/CreativeAlgo/p85_nqeen.java
+++ Practice/src/CreativeAlgo/p85_nqeen.java
@@ -8,6 +8,9 @@ public class p85_nqeen {
 
 	static int Answer;
 	static int queen;
+	static int[] checkCol;
+	static int[] checkDesc;
+	static int[] checkAsc;
 	
 	public static void main(String args[]) throws Exception {
 		
@@ -21,13 +24,41 @@ public class p85_nqeen {
 		for(int testCase = 0; testCase < T; testCase++) {
 			
 			queen = sc.nextInt();
+
+			checkCol = new int[queen];
+			checkDesc = new int[8];
+			checkAsc = new int[8];
 			
-			
+			System.out.println(dfs(1, 1));
 			
 			System.out.println("#" + testCase + " " + Answer);
 		}
-		
 	}
 	
-	
+	public static int dfs(int row, int col) {
+		
+		// 
+		
+		// col 체크, 1이면 백트레킹
+		if(checkCol[row] == 0) {
+			checkCol[row-1] = 1;
+		}
+		
+		// \ 체크, 1이면 백트레킹
+		if(checkDesc[queen-(row-col)] == 0) { // 4-(1-1)
+			checkDesc[queen-(row-col)-1] = 1;
+		}
+		
+		// / 체크, 1이면 백트레킹
+		if(checkAsc[row + col] == 0) {
+			checkAsc[row + col -1] = 1;
+		}
+		
+		
+		// 다음 row에서 탐색
+		
+		
+		
+		return 1;
+	}
 }
