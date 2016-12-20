0135e98bd40c99865d8af04387a32ee4e5b09688
gunho14.choi <gunho14.choi@samsung.com>
2016-12-20 13:30:17 +0900

    solved

--- Practice/src/pastQuestion/forest_fire_watcher.java
+++ Practice/src/pastQuestion/forest_fire_watcher.java
@@ -8,9 +8,11 @@ public class forest_fire_watcher {
 
 	static int Answer;
 	static int[][] map;
+	private static int watcher;
 
 	public static void main(String args[]) throws Exception {
 		
+//		System.setIn(new FileInputStream("C:\\forest_fire_watcher.txt"));
 		System.setIn(new FileInputStream(new File("").getAbsolutePath() + "\\res\\forest_fire_watcher.txt"));
 		
 		Scanner sc = new Scanner(System.in);
@@ -20,23 +22,58 @@ public class forest_fire_watcher {
 			
 			int row = sc.nextInt(); // 5
 			int col = sc.nextInt(); // 6
-			map = new int[row+1][col+1];
+			map = new int[row+2][col+2];
 			
+			// 2차원 배열로 묘목 지도 생성
 			for(int i=1; i<=row; i++) {
 				for(int j=1; j<=col; j++) {
 					map[i][j] = sc.nextInt();
 				}
 			}
 		
-			dfs(1);
+			Answer = 0;
 			
+			for(int i=1; i<= row; i++) {
+				for(int j=1; j<= col; j++) {
+					
+					if(map[i][j] == 1) {
+						
+						// 깊이 우선 탐색 수행
+						watcher = dfs(i, j); 
+						//System.out.println("watchers: " + watcher);
+						
+						if(watcher%2 == 1) {
+							Answer += (watcher + 1)/2;
+						} else if(watcher == 1) {
+							Answer += 1;
+						} else {
+							Answer += watcher/2;
+						}
+						
+						//System.out.println("required watcher: " + Answer);
+						watcher = 0;
+					}
+				}
+			}
 			System.out.println("#" + testCase + " " + Answer);
 		}
 	}
 	
-	public static void dfs(int row) {
+	public static int dfs(int r, int c) {
 
-		
+		if(map[r][c] == 1) {
+			//System.out.println("map: " + r + " " + c);
+			watcher++;
+			map[r][c] = 0;
+
+			if(map[r+1][c] == 1 || map[r-1][c] == 1 || map[r][c+1] == 1 || map[r][c-1] == 1) {
+				dfs(r+1, c);
+				dfs(r-1, c);
+				dfs(r, c+1);
+				dfs(r, c-1);
+			}
+		}
+		return watcher;
 	}
-
+	
 }
