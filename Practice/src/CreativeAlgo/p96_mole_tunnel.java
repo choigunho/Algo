5b12d023d39dcfe90c8c3df53565e28b03669284
gunho14.choi <gunho14.choi@samsung.com>
2016-12-21 13:38:53 +0900

    .

--- Practice/src/CreativeAlgo/p96_mole_tunnel.java
+++ Practice/src/CreativeAlgo/p96_mole_tunnel.java
@@ -1,6 +1,5 @@
 package CreativeAlgo;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.util.*;
 
@@ -9,23 +8,29 @@ public class p96_mole_tunnel {
 	static int Answer;
 	static int[][] map;
 	
+	
 	public static void main(String args[]) throws Exception {
 		
-		String path = new File("").getAbsoluteFile() + "\\res\\";
+		String path = p96_mole_tunnel.class.getResource("").getPath();
 		String fileName = "p96_mole_tunnel.txt";
-		System.setIn(new FileInputStream(path + fileName));
+		System.setIn(new FileInputStream(path + "\\" + fileName));
 		
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
 		
 		for(int testCase = 0; testCase < T; testCase++) {
 			
+			
+			
+			
+			
+			
 			System.out.println("#" + testCase + " " + Answer);
 		}
 	}
 	
 	public static void dfs(int row) {
-
 		
 	}
+	
 }
