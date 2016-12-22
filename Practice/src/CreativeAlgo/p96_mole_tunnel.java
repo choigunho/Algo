ae417253e372ffd638045f73dedf422756a93c1f
gunho14.choi <gunho14.choi@samsung.com>
2016-12-22 10:16:39 +0900

    solve

--- Practice/src/CreativeAlgo/p96_mole_tunnel.java
+++ Practice/src/CreativeAlgo/p96_mole_tunnel.java
@@ -7,29 +7,64 @@ public class p96_mole_tunnel {
 
 	static int Answer;
 	static int[][] map;
-	
+	static List<Integer> l;
+	private static int count;
 	
 	public static void main(String args[]) throws Exception {
 		
 		String path = p96_mole_tunnel.class.getResource("").getPath();
 		String fileName = "p96_mole_tunnel.txt";
-		System.setIn(new FileInputStream(path + "\\" + fileName));
+		System.setIn(new FileInputStream(path + fileName));
 		
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
 		
 		for(int testCase = 0; testCase < T; testCase++) {
 			
+			int area = sc.nextInt();
+			map = new int[area+2][area+2];
 			
+			for(int i=1; i<=area; i++) {
+				for(int j=1; j<=area; j++) {
+					map[i][j] = sc.nextInt();
+				}
+			}
 			
+			l = new ArrayList<Integer>();
+			for(int i=1; i<= area; i++) {
+				for(int j=1; j<= area; j++) {
+					if(map[i][j] == 1) {
+						dfs(i, j);
+//						System.out.println("count: " + count);
+						
+						l.add(count);
+						
+						count = 0;
+					}
+				}
+			}
 			
-			
-			
-			System.out.println("#" + testCase + " " + Answer);
+			l.sort(null);
+			Answer = l.size();
+			System.out.println("#" + testCase + " " + Answer + " " + l);
 		}
 	}
 	
-	public static void dfs(int row) {
+	public static void dfs(int r, int c) {
+		
+		if(map[r][c] == 1) {
+//			System.out.println(r + " " + c);
+			count++;
+			map[r][c] = 0;
+			
+			if(map[r+1][c] == 1 || map[r-1][c] == 1 || map[r][c+1] == 1 || map[r][c-1] == 1) {
+				dfs(r+1, c);
+				dfs(r-1, c);
+				dfs(r, c+1);
+				dfs(r, c-1);
+			}
+			
+		}
 		
 	}
 	
