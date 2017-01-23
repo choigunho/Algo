d22cf128676d540d1ac231ae235c8a6e94be8d4f
choigunho <choigunho@gunho>
2017-01-23 21:58:24 +0900

    .

---
+++ Practice/src/pastExam/number_array_20170121.java
@@ -0,0 +1,59 @@
+package pastExam;
+
+import java.io.FileInputStream;
+import java.util.*;
+
+public class number_array_20170121 {
+
+	private static int Answer;
+	private static int depth;
+	private static int N;
+	private static int[] isVisit;
+
+	public static void main(String args[]) throws Exception {
+		
+		String path = number_array_20170121.class.getResource("").getPath();
+		String fileName = "number_array_20170121.txt";
+		System.setIn(new FileInputStream(path + fileName));
+		
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for(int testCase = 1; testCase <= T; testCase++) {
+			
+			N = sc.nextInt();
+			isVisit = new int[N+1];
+			
+			depth = 1;
+			dfs(1);
+			
+			System.out.println("#" + testCase + " " + Answer);
+		}
+	}
+
+	private static void dfs(int currentNum) {
+		
+		System.out.println(currentNum + " " + depth);
+		isVisit[currentNum] = 1;
+		
+		if(depth == N) {
+			System.out.println("end!!!");
+			Answer++;
+			return;
+		}
+		
+		for(int i=1; i<=N; i++) {
+
+			if(isVisit[i] == 0) {
+				if(i == depth || i == depth-1 || i == depth+1) {
+					depth++;
+					dfs(i);
+					depth--;
+					isVisit[i] = 0;
+				
+				}
+			}
+			
+		}
+	}
+}
