41e04904547c12802cf6657ddc6d7702f723ac3f
gunho14.choi <gunho14.choi@samsung.com>
2017-02-03 09:42:01 +0900

    solved

--- Practice/src/algorithm_md/p140_remote_control.java
+++ Practice/src/algorithm_md/p140_remote_control.java
@@ -6,6 +6,8 @@ import java.util.*;
 public class p140_remote_control {
 
 	static int Answer;
+	private static int currentTemp, targetTemp;
+	private static int[] temp;
 	
 	public static void main(String args[]) throws Exception {
 		
@@ -18,12 +20,30 @@ public class p140_remote_control {
 		
 		for(int testCase = 1; testCase <= T; testCase++) {
 			
+			currentTemp = sc.nextInt();
+			targetTemp = sc.nextInt();
 			
+			Queue<int[]> q = new LinkedList<>();
+			q.add(new int[]{currentTemp, 0});
 			
-			
-			
-			
-			
+			while(!q.isEmpty()) {
+				
+				temp = q.remove();
+				System.out.println(temp[0] + " " + temp[1]);
+				
+				if(temp[0] == targetTemp) {
+					Answer = temp[1];
+					break;
+				}
+				
+				if(targetTemp > temp[0]) {
+					q.add(new int[]{temp[0]+1, temp[1]+1}); q.add(new int[]{temp[0]+5, temp[1]+1}); q.add(new int[]{temp[0]+10, temp[1]+1});					
+				} else {
+					q.add(new int[]{temp[0]-1, temp[1]+1}); q.add(new int[]{temp[0]-5, temp[1]+1}); q.add(new int[]{temp[0]-10, temp[1]+1});
+					
+				}
+				
+			}
 			
 			System.out.println("#" + testCase + " " + Answer);
 		}
